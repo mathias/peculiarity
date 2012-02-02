ed4c67fd1edd068611041d718d34fb7c84b8b252
Matt Gauger <matt.gauger@gmail.com>
2012-02-01 23:05:17 -0600

    Minor tweak to rails admin ;-)

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -18,7 +18,7 @@ RailsAdmin.config do |config|
   # config.audit_with :paper_trail, User
 
   # Set the admin name here (optional second array element will appear in a beautiful RailsAdmin red ©)
-  config.main_app_name = ['Peculiarity', 'Admin']
+  config.main_app_name = ['Peculiarity', 'Administration']
   # or for a dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
