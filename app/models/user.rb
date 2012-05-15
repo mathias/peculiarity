b9c16375fc00b54ef3c504ff08173174314d50e9
Matt Gauger <matt.gauger@gmail.com>
2012-05-14 22:20:27 -0500

    Cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   ROLES = %w[admin]
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role
