413c33b57d1aa10171c49345b73cd0dc9a2557ae
Matt Gauger <matt.gauger@gmail.com>
2012-05-14 22:32:03 -0500

    Use rails-api gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peculiarity::Application.config.secret_token = '6a779b895e7ee2a256181759370218792f03d03360b4c9f5bb4713788fc6c3a1a10b1111dc992c3765b7a45207dd71ceca47c7499b962e080ab879c9528fd881'
